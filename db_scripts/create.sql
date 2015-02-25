ce0606a790ec22c9a15eeafe33fffacdb63eb6da
pchero <pchero21@gmail.com>
2015-02-25 17:01:17 +0100

    Edited db scheme.
    - little change.

--- db_scripts/create.sql
+++ db_scripts/create.sql
@@ -1,6 +1,6 @@
-#campaign.sql
-# Created on: Aug 15, 2014
-#     Author: pchero
+--campaign.sql
+--Created on: Aug 15, 2014
+--    Author: pchero
 
 drop table if exists campaign;
 create table campaign (
@@ -56,17 +56,65 @@ create table diallist(
     primary key(seq, id)
 );
 
+-- dial list
+-- manage all of dial list tables
+--drop table if exists dial_list;
+create table dial_list(
+    seq         int(10)         unsigned auto_increment,    -- sequence
+    uuid        varchar(255)    unique,                     -- dial_list_#### reference uuid.
+    name        varchar(255),                               -- dial list name
+    detail      text,                                       -- description of dialist
+    
+    primary key(seq, uuid)
+);
+
+-- dial list original.
+-- all of other dial lists are copy of this table.
+create table dl_org(
+    seq         int(10)         unsigned auto_increment,    -- seqeuence
+    uuid        varchar(255)    unique,                     -- 
+    name        varchar(255),
+    detail      varchar(255),
+    
+    number_1    varchar(255),
+    trycnt_1    int,
+    number_2    varchar(255),
+    trycnt_2    int,
+    number_3    varchar(255),
+    trycnt_3    int,
+    number_4    varchar(255),
+    trycnt_4    int,
+    number_5    varchar(255),
+    trycnt_5    int,
+    number_6    varchar(255),
+    trycnt_6    int,
+    number_7    varchar(255),
+    trycnt_7    int,
+    number_8    varchar(255),
+    trycnt_8    int,
+
+    email       text,
+
+    call_result int,            -- call result.
+    call_detail text,           -- more detail info about call result
+    
+    primary key(seq, uuid)
+);
+
+
 -- channel. 
 -- Asterisk's peers.
-drop table if exists channel;
-create table channel(
-    seq             int(10)         unsigned auto_increment,
-    peer            varchar(255)    unique,
-    chan_type       varchar(255),
-    status          varchar(255),
-    chan_time       varchar(255),
-    address         varchar(255),
-    cause           varchar(255),
-    
-    primary key(seq)
-);
+-- drop table if exists channel;
+-- create table channel(
+--     seq             int(10)         unsigned auto_increment,
+--     peer            varchar(255)    unique,
+--     chan_type       varchar(255),
+--     status          varchar(255),
+--     chan_time       varchar(255),
+--     address         varchar(255),
+--     cause           varchar(255),
+--     
+--     primary key(seq)
+-- );
+
+
