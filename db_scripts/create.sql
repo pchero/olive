935a6706acfa4b0eb33bcb795d24e45244ebd27a
pchero <pchero21@gmail.com>
2015-05-08 16:39:42 +0200

    Small change.
    - Just fixed wrong typing.

--- db_scripts/create.sql
+++ db_scripts/create.sql
@@ -14,7 +14,6 @@ create table campaign(
     name                        varchar(255),       -- campaign name
     status      varchar(10)     default "stop",     -- status(stop/start/starting/stopping/force_stopping)
     status_code int             default 0,          -- status code(stop(0), start(1), pause(2), stopping(10), starting(11), pausing(12)
---    create_agent_uuid           varchar(255),       -- create agent uuid
     create_agent_id             varchar(255),       -- create agent uuid
     delete_agent_id             varchar(255),       -- delete agent uuid
     update_property_agent_id    varchar(255),       -- last propery update agent uuid
@@ -70,18 +69,18 @@ create table campaign_result(
     
     -- transfer info
     tr_trycnt          int,             -- transfer try count.
-    tr_agent_id      varchar(255),    -- transfered agent.
+    tr_agent_id        varchar(255),    -- transfered agent.
     tr_chan_unique_id  varchar(255),    -- trying transfer chan unique id.
     
     -- dial result
-    res_dial                varchar(255),   -- dial result(answer, no_answer, ...)\n"
-    res_answer              varchar(255),   -- AMD result.(AMDSTATUS)\n"
-    res_answer_detail       varchar(255),   -- AMD result detail.(AMDCAUSE)\n"
-    res_hangup              varchar(255),   -- hangup code.\n"
-    res_hangup_detail       varchar(255),   -- hangup detail.\n"
-    res_tr_dial             varchar(255),   -- transferred dial result(answer, no_answer, ...)\n"
-    res_tr_hangup           varchar(255),   -- hangup code.\n"
-    res_tr_hangup_detail    varchar(255),   -- hangup detail.\n"
+    res_dial                varchar(255),   -- dial result(answer, no_answer, ...)
+    res_answer              varchar(255),   -- AMD result.(AMDSTATUS)
+    res_answer_detail       varchar(255),   -- AMD result detail.(AMDCAUSE)
+    res_hangup              varchar(255),   -- hangup code.
+    res_hangup_detail       varchar(255),   -- hangup detail.
+    res_tr_dial             varchar(255),   -- transferred dial result(answer, no_answer, ...)
+    res_tr_hangup           varchar(255),   -- hangup code.
+    res_tr_hangup_detail    varchar(255),   -- hangup detail.
         
     primary key(seq, chan_unique_id)
     
@@ -119,7 +118,6 @@ create table agent(
     -- agent performance
     -- busy time, how many calls got.. Um?
     
---    primary key(uuid)
     primary key(id)
 );
 
